96204de37f536ed5f8fcadd27617711a1318853c
Issa <gamersalpha2017@gmail.com>
2024-04-20 14:08:55 +0200

    E21-Finish

--- controllers/init_weapon.gd
+++ controllers/init_weapon.gd
@@ -11,6 +11,8 @@ extends Node3D
 @onready var weapon_mesh : MeshInstance3D = %WeaponMesh
 @onready var weapon_shadow : MeshInstance3D = %WeaponShadow
 
+var mouse_movement : Vector2
+
 func _input(event):
 	if event.is_action_pressed("Weapon1"):
 		WEAPON_TYPE = load("res://models/weapon/crowbar/Crowbar.tres")
@@ -21,6 +23,8 @@ func _input(event):
 	if event.is_action_pressed("Weapon3"):
 		WEAPON_TYPE = load("res://models/weapon/M254/res-M254.tres")
 		load_weapon()
+	if event is InputEventMouseMotion:
+		mouse_movement = event.relative
 
 	Global.debug.add_property("Weapon_TYPE",WEAPON_TYPE.name, 4)
 
@@ -33,3 +37,19 @@ func load_weapon() -> void:
 	position = WEAPON_TYPE.position
 	rotation_degrees = WEAPON_TYPE.rotation
 	weapon_shadow.visible = WEAPON_TYPE.shadow
+	
+	
+func sway_weapon(delta) -> void:
+	#Clamp mouse movement
+	mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min,WEAPON_TYPE.sway_max)
+	#lerp weapon position based on mouse movement
+	position.x = lerp(position.x, WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position)* delta, WEAPON_TYPE.sway_speed_position)
+	position.y = lerp(position.y, WEAPON_TYPE.position.y - (mouse_movement.y * WEAPON_TYPE.sway_amount_position)* delta, WEAPON_TYPE.sway_speed_position)
+	
+	 #lerp weapon rotation based on mouse movement
+	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y + (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation)*delta, WEAPON_TYPE.sway_speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x + (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation)*delta, WEAPON_TYPE.sway_speed_rotation)
+
+func _physics_process(delta: float) -> void:
+	sway_weapon(delta)
+	
