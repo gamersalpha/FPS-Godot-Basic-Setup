96136149c05fe4a942b12948cf63f8114536d237
Issa <gamersalpha2017@gmail.com>
2024-04-27 16:31:27 +0200

    E20-01_Ajout de l'arme et du shadow

--- controllers/init_weapon.gd
+++ controllers/init_weapon.gd
@@ -18,12 +18,17 @@ func _input(event):
 	if event.is_action_pressed("Weapon2"):
 		WEAPON_TYPE = load("res://models/weapon/crowbar2/CrowbarL.tres")
 		load_weapon()
-
+	if event.is_action_pressed("Weapon3"):
+		WEAPON_TYPE = load("res://models/weapon/M254/Resource-M254.tres")
+		load_weapon()
+		
 func _ready() -> void:
 	load_weapon()
 
 func load_weapon() -> void:
 	weapon_mesh.mesh = WEAPON_TYPE.mesh
+	weapon_shadow.mesh = WEAPON_TYPE.mesh
 	position = WEAPON_TYPE.position
 	rotation_degrees = WEAPON_TYPE.rotation
+	scale = WEAPON_TYPE.scale
 	weapon_shadow.visible = WEAPON_TYPE.shadow
