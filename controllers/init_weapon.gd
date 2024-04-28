ddb3d7a8c272392e1f2248a55cd4feff5aa8565e
Issa <gamersalpha2017@gmail.com>
2024-04-28 19:41:50 +0200

    E21-Finich

--- controllers/init_weapon.gd
+++ controllers/init_weapon.gd
@@ -11,6 +11,12 @@ extends Node3D
 @onready var weapon_mesh : MeshInstance3D = %WeaponMesh
 @onready var weapon_shadow : MeshInstance3D = %WeaponShadow
 
+var mouse_movement : Vector2
+
+func _ready() -> void:
+	await  owner.ready
+	load_weapon()
+
 func _input(event):
 	if event.is_action_pressed("Weapon1"):
 		WEAPON_TYPE = load("res://models/weapon/crowbar/Crowbar.tres")
@@ -24,9 +30,8 @@ func _input(event):
 	if event.is_action_pressed("Weapon4"):
 		WEAPON_TYPE = load("res://models/weapon/AR160/RES_AR160.tres")
 		load_weapon()
-				
-func _ready() -> void:
-	load_weapon()
+	if event is InputEventMouseMotion:
+		mouse_movement = event.relative
 
 func load_weapon() -> void:
 	weapon_mesh.mesh = WEAPON_TYPE.mesh
@@ -35,3 +40,20 @@ func load_weapon() -> void:
 	rotation_degrees = WEAPON_TYPE.rotation
 	scale = WEAPON_TYPE.scale
 	weapon_shadow.visible = WEAPON_TYPE.shadow
+
+func sway_weapon(delta) -> void:
+	#Clamp mouse movement
+	mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min,WEAPON_TYPE.sway_max)
+	#lerp weapon position based on mouse movement
+	position.x = lerp(position.x, WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position)* delta, WEAPON_TYPE.sway_speed_position)
+	position.y = lerp(position.y, WEAPON_TYPE.position.y - (mouse_movement.y * WEAPON_TYPE.sway_amount_position)* delta, WEAPON_TYPE.sway_speed_position)
+	
+	 #lerp weapon rotation based on mouse movement
+	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y + (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation)*delta, WEAPON_TYPE.sway_speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x + (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation)*delta, WEAPON_TYPE.sway_speed_rotation)
+
+func _physics_process(delta: float) -> void:
+	sway_weapon(delta)
+
+
+
--- models/weapon/Weapons.gd
+++ models/weapon/Weapons.gd
@@ -5,6 +5,13 @@ class_name Weapons extends Resource
 @export var position : Vector3
 @export var rotation : Vector3
 @export var scale : Vector3
+@export_category("Weapon Sway")
+@export var sway_min : Vector2 = Vector2(-20.0,-20.0)
+@export var sway_max : Vector2 = Vector2(20.0,20.0)
+@export_range(0,0.2,0.01) var sway_speed_position : float = 0.07
+@export_range(0,0.2,0.01) var sway_speed_rotation : float = 0.1
+@export_range(0,0.25,0.01) var sway_amount_position : float = 0.1
+@export_range(0,50,0.1) var sway_amount_rotation : float = 30.0
 @export_category("Visual Settings")
 @export var mesh : Mesh
 @export var shadow : bool
