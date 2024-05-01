602fb6f4c63f956df0336db2072335008689ee97
Issa <gamersalpha2017@gmail.com>
2024-05-01 12:17:18 +0200

    E22-Finich

--- controllers/init_weapon.gd
+++ controllers/init_weapon.gd
@@ -7,11 +7,24 @@ extends Node3D
 		WEAPON_TYPE = value
 		if Engine.is_editor_hint():
 			load_weapon()
+@export var sway_noise : NoiseTexture2D
+@export var sway_speed : float = 1.2
+@export var reset : bool = false:
+	set(value):
+		reset = value
+		if Engine.is_editor_hint():
+			load_weapon()
 
 @onready var weapon_mesh : MeshInstance3D = %WeaponMesh
 @onready var weapon_shadow : MeshInstance3D = %WeaponShadow
 
 var mouse_movement : Vector2
+var random_sway_x
+var random_sway_y
+var random_sway_amount : float
+var time : float = 0.0
+var idle_sway_adjustement
+var idle_sway_rotation_strength
 
 func _ready() -> void:
 	await  owner.ready
@@ -32,25 +45,52 @@ func _input(event):
 		load_weapon()
 	if event is InputEventMouseMotion:
 		mouse_movement = event.relative
+	
+	Global.debug.add_property("Weapon_TYPE",WEAPON_TYPE.name, 4)
+
 
 func load_weapon() -> void:
 	weapon_mesh.mesh = WEAPON_TYPE.mesh
-	weapon_shadow.mesh = WEAPON_TYPE.mesh
 	position = WEAPON_TYPE.position
 	rotation_degrees = WEAPON_TYPE.rotation
 	scale = WEAPON_TYPE.scale
 	weapon_shadow.visible = WEAPON_TYPE.shadow
+	weapon_shadow.mesh = WEAPON_TYPE.mesh
+	idle_sway_adjustement = WEAPON_TYPE.idle_sway_adjustment
+	idle_sway_rotation_strength = WEAPON_TYPE.idle_sway_rotation_strength
+	random_sway_amount = WEAPON_TYPE.random_sway_amount
 
 func sway_weapon(delta) -> void:
-	#Clamp mouse movement
-	mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min,WEAPON_TYPE.sway_max)
-	#lerp weapon position based on mouse movement
-	position.x = lerp(position.x, WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position)* delta, WEAPON_TYPE.sway_speed_position)
-	position.y = lerp(position.y, WEAPON_TYPE.position.y - (mouse_movement.y * WEAPON_TYPE.sway_amount_position)* delta, WEAPON_TYPE.sway_speed_position)
+	if not Engine.is_editor_hint():
+		#get random sway value from 2D noise
+		var sway_random : float = get_sway_noise()
+		var sway_random_adjusted : float = sway_random * idle_sway_adjustement
+
+		time += delta * ( 1 + sway_speed + sway_random)
+		random_sway_x = sin(time *1.5 + sway_random_adjusted) / random_sway_amount
+		random_sway_y = sin(time - sway_random_adjusted) / random_sway_amount
+		
+		#Clamp mouse movement
+		mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min,WEAPON_TYPE.sway_max)
+		
+		#lerp weapon position based on mouse movement
+		position.x = lerp(position.x, WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position + random_sway_x)* delta, WEAPON_TYPE.sway_speed_position)
+		position.y = lerp(position.y, WEAPON_TYPE.position.y - (mouse_movement.y * WEAPON_TYPE.sway_amount_position + random_sway_y)* delta, WEAPON_TYPE.sway_speed_position)
+		
+		 #lerp weapon rotation based on mouse movement
+		rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y + (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation + (random_sway_y * idle_sway_rotation_strength))*delta, WEAPON_TYPE.sway_speed_rotation)
+		rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x + (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation + (random_sway_x * idle_sway_rotation_strength))*delta, WEAPON_TYPE.sway_speed_rotation)
+
+
+func get_sway_noise() -> float:
+	var player_position :Vector3 = Vector3(0,0,0)
 	
-	 #lerp weapon rotation based on mouse movement
-	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y + (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation)*delta, WEAPON_TYPE.sway_speed_rotation)
-	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x + (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation)*delta, WEAPON_TYPE.sway_speed_rotation)
+	if not Engine.is_editor_hint():
+		player_position = Global.player.global_position
+	
+	var noise_location : float = sway_noise.noise.get_noise_2d(player_position.x,player_position.y)
+	return noise_location
+
 
 func _physics_process(delta: float) -> void:
 	sway_weapon(delta)
--- models/weapon/Weapons.gd
+++ models/weapon/Weapons.gd
@@ -12,6 +12,9 @@ class_name Weapons extends Resource
 @export_range(0,0.2,0.01) var sway_speed_rotation : float = 0.1
 @export_range(0,0.25,0.01) var sway_amount_position : float = 0.1
 @export_range(0,50,0.1) var sway_amount_rotation : float = 30.0
+@export var idle_sway_adjustment : float = 10.0
+@export var idle_sway_rotation_strength : float = 300.0
+@export_range(0.1,10.0,0.1) var random_sway_amount : float = 5.0
 @export_category("Visual Settings")
 @export var mesh : Mesh
 @export var shadow : bool
